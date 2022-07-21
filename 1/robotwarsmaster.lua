13bd267ea20757715a955f4b02a3537d48938d5e
Tatounee <oettam32@gmail.com>
2022-07-22 01:08:40 +0200

    Get et traite les instr des controleurs pour les turtles

--- 1/robotwarsmaster.lua
+++ 1/robotwarsmaster.lua
@@ -16,16 +16,27 @@ for _, computer in pairs(computers) do
 	print("Computer #" .. computer)
 end
 
-function gameLoop()
+-- ------------------------ Fonction de boucle de jeu ----------------------- --
+local instructions = {}
+local function gameLoop()
 	while true do
 		rednet.broadcast("next turn", "robotwars")
+
+		for turtle_id, action in pairs(instructions) do
+			print("<" .. turtle_id .. "> do : " .. tostring(action))
+		end
+		print("============ Next turn ============")
+		instructions = {}
 
 		coroutine.yield(os.startTimer(timerInterval))
 	end
 end
 
+-- ------------------------- Démare la boucle de jeu ------------------------ --
 local timerID = os.startTimer(timerInterval)
 local Loop = coroutine.create(gameLoop)
+
+-- -------------------- Traite les requêtes sur le réseau ------------------- --
 while true do
 	local event, data, message, protocol = os.pullEventRaw()
 	if event == "terminate" then
@@ -33,6 +44,10 @@ while true do
 	elseif event == "timer" and data == timerID then
 		_, timerID = coroutine.resume(Loop)
 	elseif event == "rednet_message" then
-		print("[" .. (protocol or "\\") .. "] " .. data .. " : " .. tostring(message))
+		-- print("[" .. (protocol or "\\") .. "] " .. data .. " : " .. tostring(message))
+		if protocol == "robotwars" then
+			-- print("Message from [" .. data .. "] to <" .. players[data] .. "> : " .. tostring(message))
+			instructions[tostring(data)] = message
+		end
 	end
 end
